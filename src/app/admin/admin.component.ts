cf48c2c635796ecc704e4125e1e33e1298447842
codelaghien <codelaghien.github.io>
2021-06-18 09:13:05 -0700

    links

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { DataService } from '../data.service';
+import { Link } from '../link.model';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
+  providers: [MessageService],
 })
 export class AdminComponent implements OnInit {
+  public links: Link[] | undefined;
+  public selectedLink: Link | undefined;
+  private authorId = 1;
+  public newLink: Link | undefined;
 
-  constructor() { }
+  constructor(
+    private dataService: DataService,
+    private messageService: MessageService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public viewLink(link: Link): void {
+    console.log('view Link', link);
+    this.selectedLink = link;
   }
 
+  public getSelectedClass(link: Link): string {
+    if (!this.selectedLink) {
+      return '';
+    }
+    return this.selectedLink.id === link.id ? 'selected' : 'nonSelected';
+  }
+
+  public addLink(): void {
+    console.log('addLink');
+    this.newLink = {
+      id: 0,
+      title: '',
+      link: '',
+      author: 'Huy Nguyễn',
+      authorId: this.authorId,
+    };
+  }
+
+  public cancelAddLink(): void {
+    this.newLink = undefined;
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Thông báo',
+      detail: 'Đã hủy',
+    });
+  }
+
+  public saveLink(): void {
+    // console.log('save Link', this.newLink);
+    if (!this.newLink) {
+      return;
+    }
+    this.dataService.postLink(this.newLink).subscribe((link) => {
+      // console.log('result ', Link);
+      this.links?.push(link);
+      this.newLink = undefined;
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Thông báo',
+        detail: 'Đã thêm thành công',
+      });
+    });
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
-import { MenuItem, MessageService } from 'primeng/api';
-import { Link } from './link.model';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [MessageService],
 })
 export class AppComponent {
   title = 'Angular12PrimeNg';
   items: MenuItem[] = [];
-  public links: Link[] | undefined;
-  public selectedLink: Link | undefined;
-  private authorId = 1;
-  public newLink: Link | undefined;
 
   ngOnInit() {
     this.items = [
@@ -39,54 +33,5 @@ export class AppComponent {
         ],
       },
     ];
-  }
-
-  public viewLink(link: Link): void {
-    console.log('view Link', link);
-    this.selectedLink = link;
-  }
-
-  public getSelectedClass(link: Link): string {
-    if (!this.selectedLink) {
-      return '';
-    }
-    return this.selectedLink.id === link.id ? 'selected' : 'nonSelected';
-  }
-
-  public addLink(): void {
-    console.log('addLink');
-    this.newLink = {
-      id: 0,
-      title: '',
-      link: '',
-      author: 'Huy Nguyễn',
-      authorId: this.authorId,
-    };
-  }
-
-  public cancelAddLink(): void {
-    this.newLink = undefined;
-    this.messageService.add({
-      severity: 'info',
-      summary: 'Thông báo',
-      detail: 'Đã hủy',
-    });
-  }
-
-  public saveLink(): void {
-    // console.log('save Link', this.newLink);
-    if (!this.newLink) {
-      return;
-    }
-    this.dataService.postLink(this.newLink).subscribe((link) => {
-      // console.log('result ', Link);
-      this.links?.push(link);
-      this.newLink = undefined;
-      this.messageService.add({
-        severity: 'success',
-        summary: 'Thông báo',
-        detail: 'Đã thêm thành công',
-      });
-    });
   }
 }
