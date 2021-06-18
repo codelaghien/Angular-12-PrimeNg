aa78b29937f0d9da022578489c81a51de05019b8
codelaghien <codelaghien.github.io>
2021-06-18 09:00:32 -0700

    rut gon links

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Link } from './link.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  private REST_API_SERVER = 'http://localhost:3000';
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      // Authorization: 'my-auth-token'
+    }),
+  };
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  public getLinks(authorId: number): Observable<Link[]> {
+    const url = `${this.REST_API_SERVER}/links?authorId=` + authorId;
+    return this.httpClient.get<Link[]>(url, this.httpOptions);
+  }
+
+  public postLink(payload: Link): Observable<Link> {
+    const url = `${this.REST_API_SERVER}/links`;
+    return this.httpClient.post<Link>(url, payload, this.httpOptions);
+  }
 }
