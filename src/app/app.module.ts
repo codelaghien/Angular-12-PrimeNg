ade6675072e44a3160298375d94df9e9847c0149
codelaghien <codelaghien.github.io>
2021-06-17 17:23:07 -0700

    Notes Them tinh nang

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,6 +16,7 @@ import { SplitterModule } from 'primeng/splitter';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule,
     ButtonModule,
     CardModule,
