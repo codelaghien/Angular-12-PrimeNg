d2bf292f19c7f0b857e59a1ab871ed8d6ddfd4ba
codelaghien <codelaghien.github.io>
2021-06-18 09:16:53 -0700

    links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,6 +33,8 @@ import { AdminComponent } from './admin/admin.component';
     MessageModule,
     ConfirmDialogModule,
     ToastModule,
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
